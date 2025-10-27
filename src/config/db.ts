f69e26cc29d2fad9557324c061831448b1ee5c37
buiphukhoa2002 <buiphukhoa2002@gmail.com>
2025-10-27 18:34:10 +0700

    chore: migrate config to Typescript

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,9 +1,11 @@
-const { Pool } = require("pg");
-require("dotenv").config();
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const pool = new Pool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -13,4 +15,4 @@ const pool = new Pool({
       : false,
 });
 
-module.exports = pool;
+export default pool;
